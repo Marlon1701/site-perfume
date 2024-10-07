859eb2962138bdf0cc2ea704c66f4060738baa7b
Marlon1701 <Marlinhopereira123@gmail.com>
2024-10-07 08:43:42 -0300

    first perfume

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const sauvage = () => {
+  window.open("./pages/sauvage.html", "_self ");
+};
+
+const black = () => {
+  window.open("./pages/212black.html", "_self ");
+};
+const good = () => {
+  window.open("./pages/good.html", "_self ");
+};
+
+const scandal = () =>{
+  window.open("./pages/scandal.html", "_self ");
+};
+
+const delina = () =>{
+  window.open("./pages/delina.html", "_self ");
+}
